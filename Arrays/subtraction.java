0b86cb9a3f7bf94fa3b6ce52781f8db74747e76f
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-06-02 22:12:28 +0530

    subtraction  file modified

--- Arrays/subtraction.java
+++ Arrays/subtraction.java
@@ -3,38 +3,72 @@ import java.util.*;
 // Subtract two arrays element-wise and return the result as a new array  -> here borrow concept is not used
 
 public class subtraction {
-    static int[] sub(int[] arr1,int[] arr2){
-        int[] res= new int[arr1.length];
-
-        // element-wise subtraction
-        // method 1 
-        for(int i=0;i< arr1.length;i++){
-            res[i] = arr1[i] - arr2[i];
-        }
-
-        // for borrow concept we can use the below code - elemnt-wise subtraction with borrow
-        // method 2
-        for(int i= arr1.length-1; i>=0; i--){
-            if(res[i] < 0){
-                res[i] += 10; // borrow
-                if(i-1 >= 0){
-                    res[i-1] -= 1; 
-                }
-            }
-        }
-        return res;
+   public static int[] sub(int[] arr1, int[] arr2){
+    // Step 1: Compare arrays
+    boolean negative = false;
+    if(Arrays.compare(arr1, arr2) < 0){
+        //swap the reference
+        int[] temp = arr1;
+        arr1 = arr2;
+        arr2 = temp;
+        negative = true;
     }
+    int i = arr1.length - 1;
+    int j = arr2.length - 1;
+    int max = Math.max(arr1.length, arr2.length);
+    int[] res = new int[max];
+    int k = max - 1;
+
+    while(i >= 0 || j >= 0){
+        int a = (i >= 0) ? arr1[i] : 0; //if arr1 ends,then take remaining values as 0
+        int b = (j >= 0) ? arr2[j] : 0;//if arr2 ends,then take remaining values as 0
+
+        if(a < b){
+            // borrow
+            int borrowIndex = i - 1; //index from where we our borrowing 
+            while(borrowIndex >= 0 && arr1[borrowIndex] == 0){
+                arr1[borrowIndex] = 9;
+                borrowIndex--;
+            }
+            if(borrowIndex >= 0) {
+                arr1[borrowIndex]--;
+            }
+            a += 10; 
+        }
+
+        res[k] = a - b;
+        i--;
+        j--; 
+        k--;
+    }
+
+    // remove leading zeros
+    int start = 0;
+    while(start < res.length - 1 && res[start] == 0){
+        start++;
+    }
+
+    int ans[] = java.util.Arrays.copyOfRange(res, start, res.length); //it will return new array containing elements from res[start] to res[res.length]
+
+    //put a negative sign before final result if 1st array is smaller than second
+    if(negative){
+        System.out.print("-");
+    }
+    return ans;
+}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array: ");
+        System.out.println("Enter the size of the array1: ");
         int n = sc.nextInt();
         int[] arr1 = new int[n];
         System.out.println("Enter array elements: ");
         for(int i=0; i< n; i++){
             arr1[i] = sc.nextInt();
         }
-        int[] arr2 = new int[n];
+        System.out.println("Enter the size of the array2: ");
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
         System.out.println("Enter elements for the second array: ");
         for(int i=0; i< n; i++){
             arr2[i] = sc.nextInt();
