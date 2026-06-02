85d87ab4f1557cb0408913af43ed3755d3e3c2f1
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-06-02 22:58:09 +0530

    Add Arrays_02/Reverse.java

---
+++ Arrays_02/Reverse.java
@@ -0,0 +1,23 @@
+package Arrays_02;
+import java.util.*;
+// https://www.geeksforgeeks.org/problems/reverse-an-array/1
+public class Reverse {
+    public static void reverseArray(int arr[]) {
+        // code here
+            int left = 0;
+            int right = arr.length-1;
+            while (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,7,7,8,9};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
